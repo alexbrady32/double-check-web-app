ed55befdf383bd3afc4c50517573ccc681b0a234
Denee McClain <deneemcclain@southern.edu>
2016-09-28 16:07:21 -0400

    Added email verification and added files to project

--- DoubleCheck/DoubleCheck/Controllers/AccountController.cs
+++ DoubleCheck/DoubleCheck/Controllers/AccountController.cs
@@ -7,12 +7,15 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using DoubleCheck.Models;
+using System.Globalization;
+using System.Text.RegularExpressions;
 
 namespace DoubleCheck.Controllers
 {
     public class AccountController : Controller
     {
         private doublecheckdbEntities db = new doublecheckdbEntities();
+        private bool invalid;
 
         // GET: Account
         public ActionResult Login()
@@ -74,7 +77,7 @@ namespace DoubleCheck.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "Id,Username,Email,Password,firstName,lastName,phone_num")] User user)
         {
-            if (ModelState.IsValid)
+            if (ModelState.IsValid && IsValidEmail(user.Email))
             {
                 db.Users.Add(user);
                 db.SaveChanges();
@@ -141,5 +144,55 @@ namespace DoubleCheck.Controllers
             return RedirectToAction("Index");
         }
 
+        public bool IsValidEmail(string strIn)
+        {
+            invalid = false;
+            if (String.IsNullOrEmpty(strIn))
+                return false;
+
+            // Use IdnMapping class to convert Unicode domain names.
+            try
+            {
+                strIn = Regex.Replace(strIn, @"(@)(.+)$", this.DomainMapper,
+                                      RegexOptions.None, TimeSpan.FromMilliseconds(200));
+            }
+            catch (RegexMatchTimeoutException)
+            {
+                return false;
+            }
+
+            if (invalid)
+                return false;
+
+            // Return true if strIn is in valid e-mail format.
+            try
+            {
+                return Regex.IsMatch(strIn,
+                      @"^(?("")("".+?(?<!\\)""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
+                      @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))$",
+                      RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
+            }
+            catch (RegexMatchTimeoutException)
+            {
+                return false;
+            }
+        }
+
+        private string DomainMapper(Match match)
+        {
+            // IdnMapping class with default property values.
+            IdnMapping idn = new IdnMapping();
+
+            string domainName = match.Groups[2].Value;
+            try
+            {
+                domainName = idn.GetAscii(domainName);
+            }
+            catch (ArgumentException)
+            {
+                invalid = true;
+            }
+            return match.Groups[1].Value + domainName;
+        }
     }
 }
---
+++ DoubleCheck/DoubleCheck/Controllers/AssignmentsController.cs
@@ -0,0 +1,20 @@
+﻿using DoubleCheck.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DoubleCheck.Controllers
+{
+    public class AssignmentsController : Controller
+    {
+        private doublecheckdbEntities db = new doublecheckdbEntities();
+
+        // GET: Assignments
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
