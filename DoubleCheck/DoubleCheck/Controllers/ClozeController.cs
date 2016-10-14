ffb466ecee2ea43c925e6fa3c135c0587ad2e514
Denee McClain <deneemcclain@southern.edu>
2016-10-14 08:10:16 -0400

    Functional cloze exam, functional redirects

--- DoubleCheck/DoubleCheck/Controllers/ClozeController.cs
+++ DoubleCheck/DoubleCheck/Controllers/ClozeController.cs
@@ -23,10 +23,11 @@ namespace DoubleCheck.Controllers
         {
             return View();
         }
-
+        
         public ActionResult StopAsking()
         {
-            var user = db.Users.Where(u => u.Id == (int)Session["UserID"]).FirstOrDefault();
+            int userID = Int32.Parse((string)Session["UserID"]);
+            var user = db.Users.Where(u => u.Id == userID).FirstOrDefault();
             // Assigning the user a score that gives them an average reading level
             user.Cloze_Score = 85;
             db.SaveChanges();
@@ -35,9 +36,27 @@ namespace DoubleCheck.Controllers
 
         public ActionResult Grade()
         {
-            Console.Write("What's going on?");
-            var answer = Request.Params["paragraph-1-answers"];
-            var answer1 = Request.Params["paragraph-2-answers"];
+            int userID = Int32.Parse((string)Session["UserID"]);
+            List<String> answers = new List<string>();
+            List<string> correctAnswers = new List<string> { "D", "E", "D", "E", "D", "C", "D", "A", "C", "C"};
+            for (int i = 0; i < 10; i++)
+            {
+                if (i < 5)
+                    answers.Add(Request.Params["paragraph-" + (i + 1).ToString()+"-answers"]);
+                if (i >= 5)
+                    answers.Add(Request.Params["paragraph1-" + (i - 4).ToString() + "-answers"]);
+            }
+            decimal score = 0;
+            for (int i = 0; i < correctAnswers.Count; i++) {
+                if (answers[i] == correctAnswers[i])
+                {
+                    score++;
+                }
+            }
+            decimal grade = score / 10;
+            var user = db.Users.Where(u => u.Id == userID).FirstOrDefault();
+            user.Cloze_Score = grade * 100;
+            db.SaveChanges();
             return RedirectToAction("Index", "Home");
         }
     }
