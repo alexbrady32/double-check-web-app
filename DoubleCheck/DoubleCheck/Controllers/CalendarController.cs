e6d0ddee5ce9ee91709ce653648b54f7e59e036e
Alex Brady <alexbrady32@gmail.com>
2016-10-18 21:24:14 -0400

    Beginning to add custom class for calendar items

--- DoubleCheck/DoubleCheck/Controllers/CalendarController.cs
+++ DoubleCheck/DoubleCheck/Controllers/CalendarController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DoubleCheck.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,11 +7,20 @@ using System.Web.Mvc;
 
 namespace DoubleCheck.Controllers
 {
+    class CalendarModel
+    {
+
+    }
+
     public class CalendarController : Controller
     {
+        private doublecheckdbEntities db = new doublecheckdbEntities();
+
         // GET: Calendar
         public ActionResult Calendar()
         {
+            int userID = Int32.Parse((string)Session["UserID"]);
+            var assignments = db.Assignments.Where(u => u.U_Id == userID);
             return View();
         }
     }
