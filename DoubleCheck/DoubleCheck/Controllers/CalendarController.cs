6e02d2b291abf7d57a9baac2529828b505c4315d
Alex Brady <alexbrady32@gmail.com>
2016-10-20 22:08:41 -0400

    Populated new calendarmodel object from database. Began working on trying to pass it to the view.

--- DoubleCheck/DoubleCheck/Controllers/CalendarController.cs
+++ DoubleCheck/DoubleCheck/Controllers/CalendarController.cs
@@ -4,12 +4,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
 
 namespace DoubleCheck.Controllers
 {
-    class CalendarModel
+    public class CalendarModel
     {
-
+        public CalendarModel(string title)
+        {
+            this.title = title;
+        }
+        public string title { get; set; }
+        public DateTime startTime { get; set; }
+        public DateTime endTime { get; set; }
+        public string JsonData { get; set; }
     }
 
     public class CalendarController : Controller
@@ -19,8 +27,41 @@ namespace DoubleCheck.Controllers
         // GET: Calendar
         public ActionResult Calendar()
         {
-            int userID = Int32.Parse((string)Session["UserID"]);
-            var assignments = db.Assignments1.Where(u => u.U_Id == userID);
+            if (Session["UserID"] != null)
+            {
+                int userID = Int32.Parse((string)Session["UserID"]);
+                // TODO: Add in where clause for both assignments and classes
+                //var assignments = db.Assignments1.Where(u => u.U_Id == userID);
+                var assignments = db.Assignments1;
+                var classes = db.Classes;
+
+                var calendarData = new List<CalendarModel>();
+
+                foreach(Assignments a in assignments)
+                {
+                    CalendarModel data = new CalendarModel(a.Name);
+                    data.startTime = a.Due_Date;
+                    // Adds 30 minutes to the due date for calendar display purposes
+                    // The calendar data needs an end time so it will display properly
+                    data.endTime = a.Due_Date.AddMinutes(30);
+                    var serializer = new JavaScriptSerializer();
+                    // TODO: DO we really need the json data field in the json data?
+                    data.JsonData = serializer.Serialize(data);
+                    calendarData.Add(data);
+                }
+
+                /*
+                foreach (Class c in classes)
+                {
+                    CalendarModel data = new CalendarModel(c.Name);
+                    
+                    calendarData.Add(new CalendarModel(c.Name));
+                }
+                */
+
+                return View();
+            }
+            // TODO: need to return a not logged in page or something here.
             return View();
         }
     }
