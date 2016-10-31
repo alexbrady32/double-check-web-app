b3ca33a3d20f68f5e66016536b84d4050bf20485
Denee McClain <deneemcclain@southern.edu>
2016-10-30 20:51:16 -0400

    Adding email code; model changes

---
+++ DoubleCheck/DoubleCheck/Utilities/EmailJob.cs
@@ -0,0 +1,63 @@
+﻿using DoubleCheck.Models;
+using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net.Mail;
+using System.Net;
+
+namespace DoubleCheck.Utilities
+{
+    public class EmailJob : IJob
+    {
+        private doublecheckdbEntities db = new doublecheckdbEntities();
+        public void Execute(IJobExecutionContext context)
+        {
+            var users = db.Users.Where(u => u.canNotifyByEmail == true);
+            foreach (var user in users) {
+                var numberOfAssignments = user.Assignments.Count();
+                var totalTTC = 0;
+                foreach (var assignment in user.Assignments)
+                {
+                    totalTTC += assignment.TTC;
+                }
+                if (numberOfAssignments > 0 && totalTTC > 0)
+                {
+                    var hours = (totalTTC / 60) > 0 ? (totalTTC / 60).ToString() + " hours and " : "";
+                    var minutes = totalTTC % 60;
+                    // add link to home page in future
+                    string emailBody = "Hello, " + user.firstName.ToString() + "!\n\n"
+                        + "Don't get behind in your schoolwork! You have " + numberOfAssignments.ToString() +
+                        " assignments due this week. You have estimated that this will take you approximately " +
+                        hours + minutes.ToString() + " minutes to complete these assignments. " +
+                        "Log in to DoubleCheck to get started. \n\n" +
+                        "Sincerely, \n\n" + "Your friends at DoubleCheck";
+                    SendEmailMessage(user.Email, emailBody);
+
+                }
+            }
+        }
+
+        public void SendEmailMessage(string recipient, string body)
+        {
+            using (var message = new MailMessage("doublecheck@gmail.com", recipient))
+            {
+                message.Subject = "Assignment Reminder";
+                message.Body = body;
+                using (SmtpClient client = new SmtpClient
+                {
+                    EnableSsl = true,
+                    Host = "smtp.gmail.com",
+                    Port = 587,
+                    Credentials = new NetworkCredential("doublecheck@gmail.com", "alwaysDoubleCheck12321")
+                })
+                {
+                    client.Send(message);
+                }
+             }
+        }
+
+
+    }
+}
