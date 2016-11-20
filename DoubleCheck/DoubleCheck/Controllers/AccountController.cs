9948e2ddecb19c39fecdfab16766d4ba5c979e16
Alex Brady <alexbrady32@gmail.com>
2016-11-20 17:02:30 -0500

    Fixing merge conflicts

--- DoubleCheck/DoubleCheck/Controllers/AccountController.cs
+++ DoubleCheck/DoubleCheck/Controllers/AccountController.cs
@@ -73,6 +73,12 @@ namespace DoubleCheck.Controllers
             return View();
         }
 
+        // GET: Account/Create
+        public ActionResult ForgotPassword()
+        {
+            return View();
+        }
+
         // POST: Account/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
